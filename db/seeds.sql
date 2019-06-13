70db7e7d2919ca755680c0890297dcfb99973e1c
Paul Black <pblack67@comcast.net>
2019-06-13 12:00:21 -0500

    Created db schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO `burgers_db`.`burgers` (`burger_name`, `devoured`) VALUES ('Ultimate Bacon Burger', '0');
+INSERT INTO `burgers_db`.`burgers` (`burger_name`, `devoured`) VALUES ('Stinky Cheese Burger', '0');
+INSERT INTO `burgers_db`.`burgers` (`burger_name`, `devoured`) VALUES ('BBQ Burger', '1');
+INSERT INTO `burgers_db`.`burgers` (`burger_name`, `devoured`) VALUES ('Peanut Butter Burger', '1');
+INSERT INTO `burgers_db`.`burgers` (`burger_name`, `devoured`) VALUES ('Pork Belly Burger', '0');
