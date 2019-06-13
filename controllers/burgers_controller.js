f81d8efaefb7aebd03a155a3a24e57ab95e2cd7b
Paul Black <pblack67@comcast.net>
2019-06-13 14:57:01 -0500

    Define routes, basic burger list

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,13 +3,23 @@ const burger = require("../models/burger");
 
 let router = express.Router();
 
-router.get("/", function (request, response) {
-    // cat.all(function (data) {
-    //     var hbsObject = {
-    //         cats: data
-    //     };
-    //     console.log(hbsObject);
-    response.render("index");
+router.get("/", (request, response) => {
+    // Standard index route. Display all burgers devoured and not devoured.
+    burger.selectAll(result => {
+        response.render("index", { burgers: result });
+    });
+});
+
+router.get("/api/create", function (request, response) {
+    // Create a burger
+});
+
+router.get("/api/update/:id", function (request, response) {
+    // Update a burger
+});
+
+router.get("/api/delete/:id", function (request, response) {
+    // Create a burger
 });
 
 module.exports = router;
